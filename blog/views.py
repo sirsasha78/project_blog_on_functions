62eac8e0659131c63729bc060ac0b050fd70b426
sirsasha <krypn87@mail.ru>
2025-10-30 22:38:30 +0800

    Добавили пагинацию

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,14 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
 from .models import Post
 
 
 def post_list(request: HttpRequest) -> HttpResponse:
-    posts = Post.published.all()
+    all_posts = Post.published.all()
+    paginator = Paginator(all_posts, 3)
+    page_number = request.GET.get("page", 1)
+    posts = paginator.get_page(page_number)
     return render(request, "blog/post/list.html", {"posts": posts})
 
 
